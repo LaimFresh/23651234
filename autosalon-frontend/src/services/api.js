9f289b88876a83b6d81d80a2e42b451c6c7c6355
LaimFresh <sumav@inbox.ru>
2025-04-05 04:30:37 +0800

    добавление адресса

--- autosalon-frontend/src/services/api.js
+++ autosalon-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api', // Адрес вашего бэкенда
+  baseURL: 'https://two3651234.onrender.com/api', // Адрес вашего бэкенда
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
